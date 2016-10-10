2dcd9b81a3f93d99027f35abae93e6fa9442f005
mauro.baresic <mauro.baresic@outlook.com>
2016-10-10 15:24:46 +0200

    Added database.

--- chathost/Program.cs
+++ chathost/Program.cs
@@ -15,52 +15,52 @@ namespace ChatHost
     {
         static void Main(string[] args)
         {
-            using (SqlConnection conn = new SqlConnection())
-            {
-                conn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ChatAppConnectionString"].ConnectionString;
+            //using (SqlConnection conn = new SqlConnection())
+            //{
+            //    conn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ChatAppConnectionString"].ConnectionString;
 
-                try
-                {
-                    conn.Open();
-                    var cmd = "Select UserId, UserName from Users";
-                    using (SqlCommand sqlCommand = new SqlCommand(cmd, conn))
-                    {
-                        SqlDataReader reader = sqlCommand.ExecuteReader();
-                        if (reader.HasRows)
-                        {
-                            while (reader.Read())
-                            {
-                                Console.WriteLine("{0}\t{1}", reader.GetInt32(0),
-                                    reader.GetString(1));
-                            }
-                        }
-                        reader.Close();
-                    }
+            //    try
+            //    {
+            //        conn.Open();
+            //        var cmd = "Select UserId, UserName from Users";
+            //        using (SqlCommand sqlCommand = new SqlCommand(cmd, conn))
+            //        {
+            //            SqlDataReader reader = sqlCommand.ExecuteReader();
+            //            if (reader.HasRows)
+            //            {
+            //                while (reader.Read())
+            //                {
+            //                    Console.WriteLine("{0}\t{1}", reader.GetInt32(0),
+            //                        reader.GetString(1));
+            //                }
+            //            }
+            //            reader.Close();
+            //        }
 
-                    using (SqlCommand sqlCommand = new SqlCommand())
-                    {
-                        sqlCommand.CommandType = CommandType.StoredProcedure;
-                        sqlCommand.CommandText = "AllUsers";
-                        sqlCommand.Connection = conn;
+            //        using (SqlCommand sqlCommand = new SqlCommand())
+            //        {
+            //            sqlCommand.CommandType = CommandType.StoredProcedure;
+            //            sqlCommand.CommandText = "AllUsers";
+            //            sqlCommand.Connection = conn;
 
-                        SqlDataReader reader = sqlCommand.ExecuteReader();
-                        if (reader.HasRows)
-                        {
-                            while (reader.Read())
-                            {
-                                Console.WriteLine("{0}", reader.GetString(0));
-                            }
-                        }
-                        reader.Close();
-                    }
+            //            SqlDataReader reader = sqlCommand.ExecuteReader();
+            //            if (reader.HasRows)
+            //            {
+            //                while (reader.Read())
+            //                {
+            //                    Console.WriteLine("{0}", reader.GetString(0));
+            //                }
+            //            }
+            //            reader.Close();
+            //        }
 
-                    conn.Close();
-                }
-                catch (Exception ex)
-                {
+            //        conn.Close();
+            //    }
+            //    catch (Exception ex)
+            //    {
 
-                }
-            }
+            //    }
+            //}
 
             using (Model.ChatAppDBEntities context = new ChatAppDBEntities())
             {
