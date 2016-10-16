15e4b29b50bccebb0f83857e93ac4d63c7ccc9d7
MauroBaresic <mauro.baresic@outlook.com>
2016-10-17 01:26:05 +0200

    Added View Models.

---
+++ Common/ViewModels/ChannelVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.ViewModels
+{
+    public class ChannelVM
+    {
+        public long ChannelId { get; set; }
+        public string ChannelName { get; set; }
+
+        public override string ToString()
+        {
+            return ChannelName;
+        }
+    }
+}
---
+++ Common/ViewModels/MessageVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Common.ViewModels
+{
+    public class MessageVM
+    {
+        public long MessageId { get; set; }
+
+        public string Content { get; set; }
+
+        public string SenderUsername { get; set; }
+
+        public DateTime TimeSent { get; set; }
+
+        public override string ToString()
+        {
+            return Content;
+        }
+    }
+}
